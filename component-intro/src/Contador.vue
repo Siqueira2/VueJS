be7d35ed46b6fe559c79b6e54d01dee13346886c
Guilherme Siqueira <guilherme.siqueira@gpmp.com.br>
2023-09-01 11:47:41 -0300

    components

---
+++ component-intro/src/Contador.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="contador">
+    <span>{{ contador }}</span>
+    <button @click="adicionar">+</button>
+    <button @click="subtrair">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+
+  methods: {
+    adicionar() {
+      this.contador++;
+    },
+
+    subtrair() {
+      this.contador--;
+    },
+  },
+};
+</script>
