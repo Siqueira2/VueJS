72cec60adc74129830dd406e04cb2e18e65368f1
Guilherme Siqueira <guilherme.siqueira@gpmp.com.br>
2023-09-04 15:35:46 -0300

    todo list VueJs

---
+++ tarefas/src/components/Task.vue
@@ -0,0 +1,80 @@
+<template>
+  <div
+    class="task"
+    :class="stateClass"
+    @click.stop="$emit('stateTaskChange', task)"
+  >
+    <button class="close" @click="$emit('taskDeleted', task)">x</button>
+    <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    stateClass() {
+      return {
+        pending: this.task.pending,
+        done: !this.task.pending,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.task {
+  position: relative;
+  box-sizing: border-box;
+  width: 350px;
+  height: 150px;
+  font-weight: 300;
+  font-size: 1.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+}
+
+.done {
+  background: #4caf50;
+  border-left: 12px solid #0a8f08;
+  text-decoration: line-through;
+  color: #ddd;
+}
+
+.pending {
+  background: #f44336;
+  border-left: 12px solid #b73229;
+}
+
+.pending .close {
+  background: #b73229;
+}
+
+.done .close {
+  background: #0a8f08;
+}
+
+.close {
+  cursor: pointer;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  color: white;
+  font-weight: bold;
+  border: none;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ tarefas/src/components/TasksGrid.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="task_grid">
+    <template v-if="tasks.length">
+      <Task
+        v-for="(task, i) in tasks"
+        :key="i"
+        :task="task"
+        @taskDeleted="$emit('taskDeleted', i)"
+        @stateTaskChange="$emit('stateTaskChange', i)"
+      />
+    </template>
+    <p v-else class="no_task">Sua vida está em dia!</p>
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  components: { Task },
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.task_grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1em;
+}
+
+.no_task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
+</style>
